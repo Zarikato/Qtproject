f0777e55e4e781150ec4e3c05b0f492987dd50d7
Zarikato <91799879+Zarikato@users.noreply.github.com>
2021-10-03 14:24:52 +0400

    Update Img_create.py

--- Img_create.py
+++ Img_create.py
@@ -1,17 +1,32 @@
 from PIL import Image, ImageDraw
 
+class StarDraw():
+    def __init__(self, size, stp, color=(0, 0, 0)):
+        self.x, self.y = size
+        self.stp = stp
+        self.color = color
 
-def create_dr(x, stp):
-    im = Image.new("RGB", (x, x), (255, 255, 255))
-    drawer = ImageDraw.Draw(im)
-    for i in range(0, x // 2 + stp, stp):
+    def set_x(self, xn):
+        self.x = xn
+
+    def set_y(self, yn):
+        self.y = yn
+
+    def set_stp(self, stpn):
+        self.stp = stpn
+
+    def set_color(self, colorn):
+        self.color = colorn
+
+    def create_dr(self):
+        im = Image.new("RGB", (self.x, self.y), (255, 255, 255))
+        drawer = ImageDraw.Draw(im)
+        x = self.x
+        y = self.y
+        for i in range(0, self.y // 2 + self.stp, self.stp):
         
-        drawer.line((x // 2, i, x // 2 + i, x // 2), fill=(0, 0, 0), width=1) # I
-        drawer.line((x // 2, i, x // 2 - i, x // 2), fill=(0, 0, 0), width=1) # II
-        drawer.line((x // 2, x - i, x // 2 + i, x // 2), fill=(0, 0, 0), width=1) # III
-        drawer.line((x // 2, x - i, x // 2 - i, x // 2), fill=(0, 0, 0), width=1) # IV
-        drawer.line((0, x // 2, x, x // 2), fill=(0, 0, 0), width=1) # X0
-        drawer.line((x // 2, 0, x // 2, x), fill=(0, 0, 0), width=1) # Y0
-    im.save('image.jpg')
-
-
+            drawer.line((x // 2, i, x // 2 + i, y // 2), fill=self.color, width=1) # I
+            drawer.line((x // 2, i, x // 2 - i, y // 2), fill=self.color, width=1) # II
+            drawer.line((x // 2, y - i, x // 2 + i, y // 2), fill=self.color, width=1) # III
+            drawer.line((x // 2, y - i, x // 2 - i, y // 2), fill=self.color, width=1) # IV
+        im.save('image.jpg')
