b5f650cfe738ad484e54aa763cd41acebd3473a7
Zarikato <91799879+Zarikato@users.noreply.github.com>
2021-10-03 15:14:16 +0400

    Update Img_create.py

--- Img_create.py
+++ Img_create.py
@@ -6,11 +6,17 @@ class StarDraw():
         self.stp = stp
         self.color = color
 
-    def set_x(self, xn):
-        self.x = xn
+    def set_x(self, xn=1000):
+        if xn == '' or xn == 0:
+            xn == 1000
+        else:
+            self.x = xn
 
-    def set_y(self, yn):
-        self.y = yn
+    def set_y(self, yn=1000):
+        if yn == '' or yn == 0:
+            yn == 1000
+        else:
+            self.y = yn
 
     def set_stp(self, stpn):
         self.stp = stpn
@@ -21,12 +27,13 @@ class StarDraw():
     def create_dr(self):
         im = Image.new("RGB", (self.x, self.y), (255, 255, 255))
         drawer = ImageDraw.Draw(im)
+        colore = self.color
         x = self.x
         y = self.y
         for i in range(0, self.y // 2 + self.stp, self.stp):
         
-            drawer.line((x // 2, i, x // 2 + i, y // 2), fill=self.color, width=1) # I
-            drawer.line((x // 2, i, x // 2 - i, y // 2), fill=self.color, width=1) # II
-            drawer.line((x // 2, y - i, x // 2 + i, y // 2), fill=self.color, width=1) # III
-            drawer.line((x // 2, y - i, x // 2 - i, y // 2), fill=self.color, width=1) # IV
+            drawer.line((x // 2, i, x // 2 + i, y // 2), fill=colore, width=1) # I
+            drawer.line((x // 2, i, x // 2 - i, y // 2), fill=colore, width=1) # II
+            drawer.line((x // 2, y - i, x // 2 + i, y // 2), fill=colore, width=1) # III
+            drawer.line((x // 2, y - i, x // 2 - i, y // 2), fill=colore, width=1) # IV
         im.save('image.jpg')
